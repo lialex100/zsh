8c0a3932624d1bf3c9027561704ccd79e514fa6a
Alex Li <alex.li@rocketdt.com>
2018-04-27 10:23:10 +0800

    add alias

--- alex.zsh
+++ alex.zsh
@@ -8,8 +8,10 @@
 # brainstormr=~/Projects/development/planetargon/brainstormr
 # cd $brainstormr
 
+alias o="open ."
 alias c="clear"
 alias cls="clear"
+
 alias hc="echo "" > ~/.zsh_history & exec $SHELL -l"
 alias win='env GOOS=windows GOARCH=386 go build -v -ldflags "-s -w"'
 alias ubu='env GOOS=linux GOARCH=amd64 go build -v -ldflags "-s -w"'
